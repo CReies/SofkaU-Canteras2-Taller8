738de0a829e54395804d1e67045f1c85197c4a64
CReies <ccreyesmolina@gmail.com>
2022-05-14 22:57:43 -0500

    added Contacts and addContactForm

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,13 +1,33 @@
+import { useState } from 'react';
 import '../css/App.css';
+import AddContactForm from './AddContactForm';
+import Contacts from './Contacts';
 import Header from './Header';
 
 const App = () => {
+	const [contacts] = useState([
+		{ name: 'Juan', tel: '1231231234' },
+		{ name: 'Juan', tel: '1231231234' },
+		{ name: 'Juan', tel: '1231231234' },
+		{ name: 'Juan', tel: '1231231234' },
+		{ name: 'Juan', tel: '1231231234' },
+		{ name: 'Juan', tel: '1231231234' },
+		{ name: 'Juan', tel: '1231231234' },
+		{ name: 'Juan', tel: '1231231234' },
+	]);
+
 	return (
-		<>
+		<div className='app'>
 			<aside>
-				<Header title='Contacts' nContacts='0' />
+				<Header title='Contacts' nContacts={contacts.length} />
+				<AddContactForm />
 			</aside>
-		</>
+			<main>
+				<section>
+					<Contacts contacts={contacts} />
+				</section>
+			</main>
+		</div>
 	);
 };
 
