100d3d4ee3e248a008b59f45e6bc35a0b4b5e33d
CReies <ccreyesmolina@gmail.com>
2022-05-15 18:47:59 -0500

    a

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import { GifsContextProvider } from '../context/ContactsContext';
->>>>>>> 57443f8503f0142dcb3635e873ddd67dd54ec1a9
 import '../css/App.css';
 import AddContactForm from './AddContactForm';
 import Contacts from './Contacts';
@@ -12,10 +9,7 @@ const App = () => {
 	const nContacts = 'contacts'.length;
 
 	return (
-<<<<<<< HEAD
-=======
 		<GifsContextProvider>
->>>>>>> 57443f8503f0142dcb3635e873ddd67dd54ec1a9
 			<div className='app'>
 				<aside>
 					<Header title='Contacts' nContacts={nContacts} />
@@ -30,10 +24,7 @@ const App = () => {
 					</section>
 				</main>
 			</div>
-<<<<<<< HEAD
-=======
 		</GifsContextProvider>
->>>>>>> 57443f8503f0142dcb3635e873ddd67dd54ec1a9
 	);
 };
 
