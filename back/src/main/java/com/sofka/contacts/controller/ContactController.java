91405105dff8ec04a8dff27a867d8596905d493d
CReies <ccreyesmolina@gmail.com>
2022-05-15 17:43:36 -0500

    AllowCrossOrigin

--- back/src/main/java/com/sofka/contacts/controller/ContactController.java
+++ back/src/main/java/com/sofka/contacts/controller/ContactController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Controller cap for the Contacts
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Cristian Reyes
  */
 @RestController
+@CrossOrigin("*")
 public class ContactController {
 
   @Autowired
