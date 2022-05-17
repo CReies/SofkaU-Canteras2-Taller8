d05dc5802449bcb5859b43d1a7e7f6469b0a6e76
CReies <ccreyesmolina@gmail.com>
2022-05-16 21:06:45 -0500

    refactor

--- front/src/services/editContact.js
+++ front/src/services/editContact.js
@@ -1,13 +1,12 @@
 const editContact = async ({ id, value, info }) => {
 	const url = `http://localhost:9090/contact/${info}/${id}`;
-	console.log(value);
+	const reqData = {
+		method: 'PATCH',
+		body: value,
+	};
+
 	try {
-		var res = await fetch(url, {
-			method: 'PUT',
-			body: value,
-		});
-		var data = await res.json();
-		console.log(data);
+		await fetch(url, reqData);
 	} catch (error) {
 		console.log(error);
 	}
